f4fc1405ae6e2978316dd3e1bbb7386b57292473
Joe DI TROLIO <jditrolio@terrena.fr>
2020-05-19 22:53:20 +0200

    First commit

---
+++ Owin01_Helloworld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace Owin01_Helloworld {
+
+    class Program
+    {
+        public static void Main(string[] args) {
+
+            var url = "http://localhost:8080/";
+            var startOpts = new StartOptions(url);
+
+            using (WebApp.Start<Startup>(startOpts)) {
+                Console.WriteLine("Server run at " + url + " , press Enter to exit.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
---
+++ Owin03_Middleware/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Owin;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace Owin03_Middleware {
+
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public class Startup {
+
+        public void Configuration(IAppBuilder app) {
+
+            // Using func
+            app.Use(new Func<AppFunc, AppFunc>(next => (async env => {
+                Console.WriteLine("Middleware with AppFunc begin.");
+                await next.Invoke(env);
+                Console.WriteLine("Middleware with AppFunc end.");
+            })));
+
+            // Using IOwinContext
+            app.Use(async (context, next) => {
+                Console.WriteLine("Middleware with Lambda begin.");
+                await next();
+                Console.WriteLine("Middleware with Lambda end.");
+            });
+
+            // Use Middleware as a type
+            app.Use<LogMiddleware>();
+
+            // Use Middleware as an instance
+            var instance = new InstanceMiddleware();
+            app.Use(instance);
+
+            // Use Middleware as a type     
+            app.Use<LogOwinMiddleware>();
+
+        }
+    }
+}
+
